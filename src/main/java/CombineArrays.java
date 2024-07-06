9b4c29a4d3f53135ca1fd2ab26d4c6930d3fa74d
Gokarna Bhandari <gokarna.jung.bhandari@gmail.com>
2024-07-06 22:07:09 +0000

    CombineArray Done

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,15 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int secondArrayIndex = arr1.length;
+        int sizeOfArray = arr2.length + secondArrayIndex;
+        int[] combinedArr = new int[sizeOfArray];     
+        for(int i = 0; i < arr1.length; i++){
+            combinedArr[i] = arr1[i];
+        }
+        for(int i = secondArrayIndex; i < sizeOfArray; i++){
+            combinedArr[i] = arr2[i - secondArrayIndex];
+        }
+        return combinedArr;
     }
 }
